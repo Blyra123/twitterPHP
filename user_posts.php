68118709c6afea310fc6d2b49a01f07f139bbf46
Yra Blinov <blyra@mail.ru>
2021-12-28 18:50:06 +0400

    First commit

---
+++ user_posts.php
@@ -0,0 +1,21 @@
+<?php 
+ include_once "includes/functions.php";
+ $error = get_error_message();
+ $id = 0;
+
+ if (isset($_SESSION['user']['id'])) {
+     $id = $_SESSION['user']['id'];     
+ } else if (isset($_GET['id'])&& !empty($_GET['id'])) {
+     $id = $_GET['id'];
+ } else {
+     $id = 0;
+ }
+ 
+ $posts = get_posts($id);
+ $title = 'Твиты пользователея';
+ if (!empty($posts)) $title = 'Твиты пользователя @'.$posts[0]['login'];
+
+ include_once "includes/header.php"; 
+ if (logged_in()) include 'includes/tweet_form.php';
+ include_once "includes/posts.php";
+ include_once "includes/footer.php"; 
